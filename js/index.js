0c5c88d91c9d66548afd8276c2f48126b3a44b70
Unknown <matevidal@hotmail.com.ar>
2019-11-01 15:32:19 -0300

    cambios finales 1/11

--- js/index.js
+++ js/index.js
@@ -73,4 +73,22 @@ window.onload = function() {
       ul.innerHTML += a;
     }
   })
+
+  fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=64473b4750029f7eee1095d5f01e52e7&language=en-US")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(respuesta) {
+    console.log(respuesta)
+    // console.log(respuesta.genres[0].name);
+    var listadoGeneros = document.querySelector("#listado-de-generos");
+    for(var i = 0; i < respuesta.genres.length; i++) {
+      listadoGeneros.innerHTML += "<li><a href='generos.html?genero=" + respuesta.genres[i].id + "'>" + respuesta.genres[i].name + "</a></li>";
+    }
+  })
+
+  // https://api.themoviedb.org/3/tv/popular?api_key=64473b4750029f7eee1095d5f01e52e7&language=en-US&page=1&with_genres=ID_DEL_GENERO
+  // Las series mas populares con un genero determinado
+
+  
 }
