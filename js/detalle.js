c097b31f69181722c7120fcd47ff40970bba5bd6
David <davidzir.2001@gmail.com>
2019-11-08 10:06:29 -0300

    genero en detalles

--- js/detalle.js
+++ js/detalle.js
@@ -70,4 +70,16 @@ for (var i = 0; i < data.created_by.length; i++) {
       ul.innerHTML += a;
 
 }  })
+fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=64473b4750029f7eee1095d5f01e52e7&language=en-US")
+.then(function(response) {
+  return response.json();
+})
+.then(function(respuesta) {
+  console.log(respuesta)
+  // console.log(respuesta.genres[0].name);
+  var listadoGeneros = document.querySelector("#listado-de-generos");
+  for(var i = 0; i < respuesta.genres.length; i++) {
+    listadoGeneros.innerHTML += "<li><a href='generos.html?genero=" + respuesta.genres[i].id + "'>" + respuesta.genres[i].name + "</a></li>";
+  }
+})
 }
