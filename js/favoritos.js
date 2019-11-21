8756e24ed782a4fc4bc02a5f1e763df6cf0247d2
Ignacio Estivill <iestivill@udesa.edu.ar>
2019-11-21 18:18:10 -0300

    LOGIN Banda Cambios

--- js/favoritos.js
+++ js/favoritos.js
@@ -15,56 +15,67 @@ window.onload = function() {
     }
   })
 
+if (localStorage.getItem("usuario")!= null){
+  // favoritos
+    //Paso 1: Leo Storage
 
-// favoritos
-  //Paso 1: Leo Storage
+    var recuperoStorage = localStorage.getItem("seriesFavoritas");
 
-  var recuperoStorage = localStorage.getItem("seriesFavoritas");
+    // Si todavía no tenía series favoritos
+    if (recuperoStorage == null) {
+      // Creo una lista vacia
+      var seriesFavoritas = [];
+    } else {
+      // Descomprimo el TEXTO que tenia en storage en el array que necesito trabajar
+    var seriesFavoritas = JSON.parse(recuperoStorage);
+    }
 
-  // Si todavía no tenía series favoritos
-  if (recuperoStorage == null) {
-    // Creo una lista vacia
-    var seriesFavoritas = [];
-  } else {
-    // Descomprimo el TEXTO que tenia en storage en el array que necesito trabajar
-  var seriesFavoritas = JSON.parse(recuperoStorage);
-  }
+    for (var i = 0; i < seriesFavoritas.length; i++) {
 
-  for (var i = 0; i < seriesFavoritas.length; i++) {
-
-    fetch("https://api.themoviedb.org/3/tv/"+ seriesFavoritas[i] +"?api_key=64473b4750029f7eee1095d5f01e52e7&language=en-US")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(respuesta) {
-      console.log(respuesta);
-      var pelis = respuesta;
-      var prepath = 'https://image.tmdb.org/t/p/original/'
-      // console.log(pelis);
-      var ul = document.querySelector(".slide-seriesFavoritas")
-      console.log(ul);
-      // for (var i = 0; i < pelis.length; i++) {
-        var a = '<a href="detalle.html?id='+ pelis.id + '">'
-        a += '<li>'
-        a += '<img src="'+prepath+pelis.poster_path+'" alt="">'
-        a += '<div class="uk-position-center uk-panel"><h1>'+pelis.name.toUpperCase()+'</h1></div>'
-        a += '</li>'
-        a += '</a>'
-        ul.innerHTML += a;
-      // }
-      // for (var i = 0; i < seriesFavoritas.length; i++) {
-        //   // BUSCAR ESA SERIE Y MOSTRARLO
-        //   fetch("" + seriesFavoritas[i] + "")
-        //     .then(function(response) {
-          //       return response.json();
-          //     })
-          //     .then(function(serie) {
-            //       document.querySelector("ul").innerHTML += "<li><h3><a href=" + serie.data.id + ">" + serie.data.title + "</a></h3><img src=" + seria.data.images.original.url + "></li>";
+      fetch("https://api.themoviedb.org/3/tv/"+ seriesFavoritas[i] +"?api_key=64473b4750029f7eee1095d5f01e52e7&language=en-US")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(respuesta) {
+        console.log(respuesta);
+        var pelis = respuesta;
+        var prepath = 'https://image.tmdb.org/t/p/original/'
+        // console.log(pelis);
+        var ul = document.querySelector(".slide-seriesFavoritas")
+        console.log(ul);
+        // for (var i = 0; i < pelis.length; i++) {
+          var a = '<a href="detalle.html?id='+ pelis.id + '">'
+          a += '<li>'
+          a += '<img src="'+prepath+pelis.poster_path+'" alt="">'
+          a += '<div class="uk-position-center uk-panel"><h1>'+pelis.name.toUpperCase()+'</h1></div>'
+          a += '</li>'
+          a += '</a>'
+          ul.innerHTML += a;
+        // }
+        // for (var i = 0; i < seriesFavoritas.length; i++) {
+          //   // BUSCAR ESA SERIE Y MOSTRARLO
+          //   fetch("" + seriesFavoritas[i] + "")
+          //     .then(function(response) {
+            //       return response.json();
             //     })
-            // }
-          })
+            //     .then(function(serie) {
+              //       document.querySelector("ul").innerHTML += "<li><h3><a href=" + serie.data.id + ">" + serie.data.title + "</a></h3><img src=" + seria.data.images.original.url + "></li>";
+              //     })
+              // }
+            })
+
+
+
+}
+
+
   }
 
+else {
 
+  alert("Hace login para tener favoritos!")
+  location.href = "login.html"
+
+}
 
 }
--- js/login.js
+++ js/login.js
@@ -1,49 +1,95 @@
-function validar() {var usuario, email;
-  usuario=document.getElementById("usuario").value;
-    if (usuario==="")
-    alert("El campo está vacío!");
-  }
+sessionStorage.setItem("userId", id)
 
-  document.querySelector("form").addEventListener("sumbit",function(event){
-      event.preventDefault()
+
+
+var userId = {
+email: "",
+userName: "",
+password: ""
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function validar() {var usuario, email;
+//  usuario=document.getElementById("usuario").value;
+  //  if (usuario==="")
+//    alert("El campo está vacío!");
+//  }
+
+//  document.querySelector("form").addEventListener("sumbit",function(event){
+//      event.preventDefault()
 
       // Tiene usuario?
 
-      var usuario = window.localStorage.getItem("usuario");
-        console.log(usuario);
+  //    var usuario = window.localStorage.getItem("usuario");
+  //      console.log(usuario);
 
       //Si tiene, comparo con el que ingresó
       //Es igual al ingresado, remplazo login por el nombre
 
-      var nuevoUsuario = window.localStorage.getElementById("usuario");
-      console.log(nuevoUsuario)
-if (nuevoUsuario === usuario){
+  //    var nuevoUsuario = window.getElementById("usuario");
+//      console.log(nuevoUsuario)
+//if (nuevoUsuario === usuario){
 
   //innerHTML capturo login y lo remplazo
 
-  console.log("Son Iguales!");
-  document.getElementById("login").innerText=nuevoUsuario
-}
+  //console.log("Son Iguales!");
+  //document.getElementById("login").innerText=nuevoUsuario
+//}
 
-else {
+//else {
   //guardar dato en local storage, set items
-  registerUser(nuevoUsuario)
-  console.log("usuario registrado");
-}
+//  registerUser(nuevoUsuario)
+  //console.log("usuario registrado");
+//}
 
-function registerUser(usuario){
+//function registerUser(usuario){
 
-window.localStorage.setItem("usuario", nuevoUsuario);
-return `New user ${nuevoUsuario} now registered`;
-}
+//window.localStorage.setItem("usuario", nuevoUsuario);
+//return `New user ${nuevoUsuario} now registered`;
+//}
 
 //sacar Modal (pop-up)
-var modal = document.getElementById("id01");
+//var modal = document.getElementById("id01");
 
 //salir del pop up cuando apretas afuera del mismo
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none"
-  }
-}
-  })
+//window.onclick = function(event) {
+  //if (event.target == modal) {
+  //  modal.style.display = "none"
+//  }
+//}
+//  })
