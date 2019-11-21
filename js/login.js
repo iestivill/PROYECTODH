de375cafe7ddcd095729644f0a464dea3c943abb
Ignacio Estivill <iestivill@udesa.edu.ar>
2019-11-21 17:21:17 -0300

    login js empezado

--- js/login.js
+++ js/login.js
@@ -3,3 +3,47 @@ function validar() {var usuario, email;
     if (usuario==="")
     alert("El campo está vacío!");
   }
+
+  document.querySelector("form").addEventListener("sumbit",function(event){
+      event.preventDefault()
+
+      // Tiene usuario?
+
+      var usuario = window.localStorage.getItem("usuario");
+        console.log(usuario);
+
+      //Si tiene, comparo con el que ingresó
+      //Es igual al ingresado, remplazo login por el nombre
+
+      var nuevoUsuario = window.localStorage.getElementById("usuario");
+      console.log(nuevoUsuario)
+if (nuevoUsuario === usuario){
+
+  //innerHTML capturo login y lo remplazo
+
+  console.log("Son Iguales!");
+  document.getElementById("login").innerText=nuevoUsuario
+}
+
+else {
+  //guardar dato en local storage, set items
+  registerUser(nuevoUsuario)
+  console.log("usuario registrado");
+}
+
+function registerUser(usuario){
+
+window.localStorage.setItem("usuario", nuevoUsuario);
+return `New user ${nuevoUsuario} now registered`;
+}
+
+//sacar Modal (pop-up)
+var modal = document.getElementById("id01");
+
+//salir del pop up cuando apretas afuera del mismo
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none"
+  }
+}
+  })
