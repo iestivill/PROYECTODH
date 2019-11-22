c7ef8245cd131d8513f30350ac8e406571c7f93c
Ignacio Estivill <iestivill@udesa.edu.ar>
2019-11-21 23:32:37 -0300

    fav CSS

--- js/favoritos.js
+++ js/favoritos.js
@@ -24,14 +24,15 @@ window.onload = function() {
     // Si todavía no tenía series favoritos
     if (recuperoStorage == null) {
       // Creo una lista vacia
-      var seriesFavoritas = [];
-    } else {
+      var seriesFavoritas = [];  }
+
+    else {
       // Descomprimo el TEXTO que tenia en storage en el array que necesito trabajar
     var seriesFavoritas = JSON.parse(recuperoStorage);
     }
 
     for (var i = 0; i < seriesFavoritas.length; i++) {
-
+//por cada serie favorita seleccionada a través del boton en detalles, tengo un fetch:
       fetch("https://api.themoviedb.org/3/tv/"+ seriesFavoritas[i] +"?api_key=64473b4750029f7eee1095d5f01e52e7&language=en-US")
       .then(function(response) {
         return response.json();
@@ -40,28 +41,23 @@ window.onload = function() {
         console.log(respuesta);
         var pelis = respuesta;
         var prepath = 'https://image.tmdb.org/t/p/original/'
-        // console.log(pelis);
+  //crea la variable "ul", que corresponde al carrousel llamado ".slide-seriesFavoritas" en el html de "favoritos"
         var ul = document.querySelector(".slide-seriesFavoritas")
+// Este console.log me muestra todo lo que hay dentro del "ul"
         console.log(ul);
-        // for (var i = 0; i < pelis.length; i++) {
+//+= agrega HTML dentro de la variable "a", pero sin pisar lo que habia antes cada vez que se ejecuta el fetch
           var a = '<a href="detalle.html?id='+ pelis.id + '">'
           a += '<li>'
           a += '<img src="'+prepath+pelis.poster_path+'" alt="">'
           a += '<div class="uk-position-center uk-panel"><h1>'+pelis.name.toUpperCase()+'</h1></div>'
           a += '</li>'
           a += '</a>'
+
+// INSERTAR el "a" dentro de nuestra variable "ul"
           ul.innerHTML += a;
-        // }
-        // for (var i = 0; i < seriesFavoritas.length; i++) {
-          //   // BUSCAR ESA SERIE Y MOSTRARLO
-          //   fetch("" + seriesFavoritas[i] + "")
-          //     .then(function(response) {
-            //       return response.json();
-            //     })
-            //     .then(function(serie) {
-              //       document.querySelector("ul").innerHTML += "<li><h3><a href=" + serie.data.id + ">" + serie.data.title + "</a></h3><img src=" + seria.data.images.original.url + "></li>";
-              //     })
-              // }
+
+
+
             })
 
 
